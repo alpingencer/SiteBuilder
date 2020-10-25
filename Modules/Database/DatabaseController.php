1f521c70e3f386bd3e1e08287e7797f53f229455
Alpin Gencer <alpin@gencer.us>
2020-10-25 10:16:40 +0100

    Initial commit for modules

---
+++ Modules/Database/DatabaseController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace SiteBuilder\Modules\Database;
+
+
+abstract class DatabaseController {
+	private $server;
+	private $name;
+	private $user;
+	private $password;
+	private $isLoggingEnabled;
+	private $logTableName;
+
+	public final static function init(string $server, string $name, string $user, string $password): DatabaseController {
+		return new static($server, $name, $user, $password);
+	}
+
+	private final function __construct(string $server, string $name, string $user, string $password) {
+		$this->setServer($server);
+		$this->setName($name);
+		$this->setUser($user);
+		$this->setPassword($password);
+		$this->setLoggingEnabled(false);
+	}
+
+	public abstract function connect(): void;
+
+	public abstract function getRow(string $table, string $id, string $columns = '*', string $primaryKey = 'ID'): array;
+
+	public abstract function getRows(string $table, string $where, string $columns = '*', string $order = ''): array;
+
+	public abstract function getVal(string $table, string $id, string $column, string $primaryKey = 'ID'): string;
+
+	public abstract function insert(string $table, array $values, $primaryKey = 'ID'): int;
+
+	public abstract function update(string $table, array $values, string $where): int;
+
+	public abstract function delete(string $table, string $where): int;
+
+	public abstract function log(string $type, string $query): bool;
+
+	public final function getServer(): string {
+		return $this->server;
+	}
+
+	private final function setServer($server): self {
+		$this->server = $server;
+		return $this;
+	}
+
+	public final function getName(): string {
+		return $this->name;
+	}
+
+	private final function setName($name): self {
+		$this->name = $name;
+		return $this;
+	}
+
+	public final function getUser(): string {
+		return $this->user;
+	}
+
+	private final function setUser($user): self {
+		$this->user = $user;
+		return $this;
+	}
+
+	public final function getPassword(): string {
+		return $this->password;
+	}
+
+	private final function setPassword($password): self {
+		$this->password = $password;
+		return $this;
+	}
+
+	public final function isLoggingEnabled(): bool {
+		return $this->isLoggingEnabled;
+	}
+
+	public function setLoggingEnabled(bool $isLoggingEnabled, string $logTableName = '__log'): self {
+		$this->isLoggingEnabled = $isLoggingEnabled;
+
+		if($this->isLoggingEnabled) {
+			$this->setLogTableName($logTableName);
+		} else {
+			unset($this->logTableName);
+		}
+
+		return $this;
+	}
+
+	public final function getLogTableName(): string {
+		return $this->logTableName;
+	}
+
+	private final function setLogTableName(string $logTableName): self {
+		$this->logTableName = $logTableName;
+		return $this;
+	}
+
+}
+
---
+++ Modules/Database/DatabaseModule.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SiteBuilder\Modules\Database;
+
+use SiteBuilder\Core\MM\Module;
+use ErrorException;
+
+class DatabaseModule extends Module {
+	private $database;
+
+	public function init(array $config): void {
+		if(!isset($config['class'])) $config['class'] = MySQLDatabaseController::class;
+
+		$requiredConfigParams = [
+				'server',
+				'name',
+				'user',
+				'password'
+		];
+
+		// Check if each required parameters is set
+		// If no, throw error: The parameter must be defined
+		foreach($requiredConfigParams as $param) {
+			if(!isset($config[$param])) {
+				throw new ErrorException("The required configuration parameter '$param' has not been set!");
+			}
+		}
+
+		// Initaite class and connect to database
+		$this->database = call_user_func(array(
+				$config['class'],
+				'init'
+		), $config['server'], $config['name'], $config['user'], $config['password']);
+		$this->database->connect();
+	}
+
+	public function getDatabase(): DatabaseController {
+		return $this->database;
+	}
+
+	public function db(): DatabaseController {
+		return $this->getDatabase();
+	}
+
+}
+
