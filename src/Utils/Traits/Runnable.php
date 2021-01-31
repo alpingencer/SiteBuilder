aa5b8a1475c60da9663031c4a85d56e20d941258
Alpin Gencer <alpin@gencer.us>
2021-01-31 12:48:05 +0100

    Refactor Runnable.php with extended testing

--- src/Utils/Traits/Runnable.php
+++ src/Utils/Traits/Runnable.php
@@ -7,8 +7,8 @@
 
 namespace SiteBuilder\Utils\Traits;
 
-use LogicException;
-use ValueError;
+use BadMethodCallException;
+use UnexpectedValueException;
 
 trait Runnable {
 	private int $currentRunStage;
@@ -20,26 +20,24 @@ trait Runnable {
 
 	private function assertCurrentRunStage(int $run_stage): void {
 		// Call to getter function to set run stage initially
-		$this->currentRunStage();
+		$current_stage = $this->currentRunStage();
 
-		// Assert the given run stage is greater than 0: Run stages are 1-indexed
+		// Assert that the given run stage is greater than 0: Run stages are 1-indexed
 		assert(
 			$run_stage > 0,
-			new ValueError("The given run stage must be greater than 0!")
+			new UnexpectedValueException("Forbidden run stage value: Run stage must be greater than 0")
 		);
 
-		// Assert run stage wasn't run previously: Cannot re-run run stages
-		$current_stage = $this->currentRunStage;
+		// Assert that the run stage wasn't run previously: Cannot re-run run stages
 		assert(
 			$this->currentRunStage <= $run_stage,
-			new LogicException("Cannot re-run the stage #$run_stage! Currently on stage #$current_stage")
+			new BadMethodCallException("Forbidden call to run stage #$run_stage: Already ran (currently on stage #$current_stage)")
 		);
 
-		// Assert run stage doesn't skip ahead: Must run run stages in order
-		$current_stage = $this->currentRunStage;
+		// Assert that the run stage doesn't skip ahead: Must run run stages in order
 		assert(
 			$this->currentRunStage >= $run_stage,
-			new LogicException("Cannot skip to the run stage #$run_stage! Currently on stage #$current_stage")
+			new BadMethodCallException("Forbidden call to run stage #$run_stage! Cannot skip ahead (currently on stage #$current_stage)")
 		);
 
 		// Increment to next run stage
