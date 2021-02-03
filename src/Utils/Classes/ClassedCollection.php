811be34fc4efa3269a7a2580739927760a687942
Alpin Gencer <alpin@gencer.us>
2021-02-03 14:47:02 +0100

    Add missing copyright headers

--- src/Utils/Classes/ClassedCollection.php
+++ src/Utils/Classes/ClassedCollection.php
@@ -1,4 +1,9 @@
 <?php
+/**************************************************
+ *          The SiteBuilder PHP Framework         *
+ *         Copyright (c) 2021 Alpin Gencer        *
+ *      Refer to LICENSE.md for a full notice     *
+ **************************************************/
 
 namespace SiteBuilder\Utils\Classes;
 
