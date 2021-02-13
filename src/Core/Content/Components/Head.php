7bc96131ed44e1321f1d6e10462e5b9bf169629d
Alpin Gencer <alpin@gencer.us>
2021-02-13 19:11:18 +0100

    Add missing copyright headers

--- src/Core/Content/Components/Head.php
+++ src/Core/Content/Components/Head.php
@@ -1,4 +1,9 @@
 <?php
+/**************************************************
+ *            The Eufony PHP Framework            *
+ *         Copyright (c) 2021 Alpin Gencer        *
+ *      Refer to LICENSE.md for a full notice     *
+ **************************************************/
 
 namespace Eufony\Core\Content\Components;
 
