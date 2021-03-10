382a54a5ecda3098c20fb8d65eabcefc34192016
Alpin Gencer <alpingencer@gmail.com>
2021-03-10 17:12:52 +0100

    Add secondary manager object support to ManagedObject

--- src/Utils/Traits/ManagedObject.php
+++ src/Utils/Traits/ManagedObject.php
@@ -14,25 +14,30 @@ use UnexpectedValueException;
 trait ManagedObject {
 	private ?object $manager;
 
-	public function manager(): ?object {
-		$this->manager ??= null;
-		return $this->manager;
+	private function manager(object|string $manager = null): ?object {
+		if($manager === null) {
+			return $this->manager
+				// Assert that the manager has been set: Cannot return manager if not set
+				?? throw new UnexpectedValueException("Cannot return manager: Manager has not been set");
+		} else {
+			if(is_string($manager)) {
+				try {
+					$manager_class = $manager;
+					/** @var $manager Singleton */
+					$manager = $manager::instance();
+				} catch(Error) {
+					throw new UnexpectedValueException("Forbidden manager class: The given class '$manager_class' must be a Singleton");
+				} catch(BadMethodCallException) {
+					throw new BadMethodCallException("Forbidden manager instance: The given singleton manager '$manager_class' has not been initialized");
+				}
+			}
+
+			return $manager;
+		}
 	}
 
 	private function setManager(object|string $manager): static {
-		if(is_string($manager)) {
-			try {
-				$manager_class = $manager;
-				/** @var $manager Singleton */
-				$manager = $manager::instance();
-			} catch(Error) {
-				throw new UnexpectedValueException("Forbidden manager class: The given class '$manager_class' must be a singleton");
-			} catch(BadMethodCallException) {
-				throw new BadMethodCallException("Forbidden manager instance: The given singleton manager '$manager_class' has not been initialized");
-			}
-		}
-
-		$this->manager = $manager;
+		$this->manager = $this->manager($manager);
 		return $this;
 	}
 
@@ -41,16 +46,7 @@ trait ManagedObject {
 		$this->assertCallerIsManager();
 	}
 
-	private function assertManagerIsset(): void {
-		// Assert that the manager has been set: Cannot assert caller if manager is unknown
-		if($this->manager() === null) {
-			throw new UnexpectedValueException("Cannot assert caller is manager: Manager has not been set");
-		}
-	}
-
-	private function assertCallerIsManager(): void {
-		$this->assertManagerIsset();
-
+	private function assertCallerIsManager(object|string $manager = null): void {
 		// Get the call trace as array
 		$trace = debug_backtrace();
 		$iteration = 0;
@@ -64,7 +60,7 @@ trait ManagedObject {
 		$caller = $trace[$iteration]['object'] ?? null;
 
 		// Assert that the method call was from the manager: Object must be managed by manager
-		if($caller !== $this->manager) {
+		if($caller !== $this->manager($manager)) {
 			$method = $trace[$iteration - 1]['function'];
 			throw new BadMethodCallException("Forbidden call to method '" . static::class . "::$method()': Method must be called by the object's manager");
 		}
